486a2f5e53c805e4a25259034148ea98ce510ca8
Jeremy Jackson <jeremy.jackson@voyant.com>
2018-12-27 10:16:57 -0700

    Corrected linting errors for `prefer-const`.

--- test/winston-graylog2-test.js
+++ test/winston-graylog2-test.js
@@ -7,7 +7,7 @@ const WinstonGraylog2 = require('../lib/winston-graylog2.js');
 describe('winstone-graylog2', function() {
   describe('Creating the trasport', function() {
     it('should have default properties when instantiated', function() {
-      let winstonGraylog2 = new WinstonGraylog2();
+      const winstonGraylog2 = new WinstonGraylog2();
 
       assert.ok(winstonGraylog2.name === 'graylog2');
       assert.ok(winstonGraylog2.level === undefined);
@@ -24,7 +24,7 @@ describe('winstone-graylog2', function() {
     });
 
     it('should allow properties to be set when instantiated', function() {
-      let options = {
+      const options = {
         name: 'not-default',
         level: 'not-default',
         graylog: {
@@ -36,7 +36,7 @@ describe('winstone-graylog2', function() {
           ],
         },
       };
-      let winstonGraylog2 = new WinstonGraylog2(options);
+      const winstonGraylog2 = new WinstonGraylog2(options);
 
       assert.ok(winstonGraylog2.name === options.name);
       assert.ok(winstonGraylog2.level === options.level);
@@ -44,29 +44,29 @@ describe('winstone-graylog2', function() {
     });
 
     it('should allow Winston properties to be set when instantiated', function() {
-      let options = {
+      const options = {
         handleExceptions: true,
         exceptionsLevel: 'not-default',
       };
-      let winstonGraylog2 = new WinstonGraylog2(options);
+      const winstonGraylog2 = new WinstonGraylog2(options);
 
       assert.ok(winstonGraylog2.handleExceptions === options.handleExceptions);
       assert.ok(winstonGraylog2.exceptionsLevel === options.exceptionsLevel);
     });
 
     it('should have a log function', function() {
-      let winstonGraylog2 = new WinstonGraylog2();
+      const winstonGraylog2 = new WinstonGraylog2();
       assert.ok(typeof winstonGraylog2.log === 'function');
     });
 
     it('should have prelog function', function() {
-      let winstonGraylog2 = new WinstonGraylog2();
+      const winstonGraylog2 = new WinstonGraylog2();
       assert.ok(typeof winstonGraylog2.prelog === 'function');
     });
 
     it('should have filter by prelog function', function(done) {
-      let msg = 'test';
-      let winstonGraylog2 = new WinstonGraylog2();
+      const msg = 'test';
+      const winstonGraylog2 = new WinstonGraylog2();
       winstonGraylog2.graylog2.info = function(data) {
         assert.ok(msg === data);
         done();
@@ -75,8 +75,8 @@ describe('winstone-graylog2', function() {
     });
 
     it('should be able to set prelog function', function(done) {
-      let msg = '  test  ';
-      let winstonGraylog2 = new WinstonGraylog2({
+      const msg = '  test  ';
+      const winstonGraylog2 = new WinstonGraylog2({
         prelog: function(msg) {
           return msg.trim();
         },
@@ -89,7 +89,7 @@ describe('winstone-graylog2', function() {
     });
 
     it('can be registered as winston transport', function() {
-      let logger = new winston.Logger({
+      const logger = new winston.Logger({
         exitOnError: false,
         transports: [new WinstonGraylog2()],
       });
@@ -98,7 +98,7 @@ describe('winstone-graylog2', function() {
     });
 
     it('can be registered as winston transport using the add() function', function() {
-      let logger = new winston.Logger({
+      const logger = new winston.Logger({
         exitOnError: false,
         transports: [],
       });
@@ -109,7 +109,7 @@ describe('winstone-graylog2', function() {
     });
 
     it('should set graylog configuration', function() {
-      let graylogOptions = {
+      const graylogOptions = {
         servers: [
           {
             host: 'somehost',
@@ -117,20 +117,20 @@ describe('winstone-graylog2', function() {
           },
         ],
       };
-      let winstonGraylog2 = new WinstonGraylog2({
+      const winstonGraylog2 = new WinstonGraylog2({
         graylog: graylogOptions,
       });
       assert.deepEqual(winstonGraylog2.graylog, graylogOptions);
     });
 
     it('should have a processMeta function', function() {
-      let winstonGraylog2 = new WinstonGraylog2();
+      const winstonGraylog2 = new WinstonGraylog2();
       assert.ok(typeof winstonGraylog2.processMeta === 'function');
     });
 
     it('should be able to set the processMeta function', function() {
-      let extension = {foo: 'bar'};
-      let winstonGraylog2 = new WinstonGraylog2({
+      const extension = {foo: 'bar'};
+      const winstonGraylog2 = new WinstonGraylog2({
         processMeta: function(meta) {
           meta.testAttribute = extension;
           delete meta.baz;
