207469a32b99195048ba8e93943d01ed96db7d98
Vyacheslav Ruzanov <spb.web@bk.ru>
2019-07-08 00:54:54 +0400

    fixed index.d.ts
    Type 'typeof TransportStream' is not a constructor function type.

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import * as TransportStream from "winston-transport";
+import TransportStream from "winston-transport";
 
 declare class Graylog2Transport extends TransportStream {
   constructor(options?: Graylog2Transport.TransportOptions);
